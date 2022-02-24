4a6903d0710bf78846bb4f7c8ad94fb6ff18053a
web-qlalf <web_qlalf@naver.com>
2022-02-25 00:02:12 +0900

    SpringBootServletInitializer 설명 추가

--- myMall/src/main/java/mall/MyMallApplication.java
+++ myMall/src/main/java/mall/MyMallApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @EntityScan(basePackages = {"mall.*"})
 @SpringBootApplication(scanBasePackages = "mall.*")
 public class MyMallApplication extends SpringBootServletInitializer{
+	// SpringBootServletInitializer 없으면 jsp 출력 안 됨
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyMallApplication.class, args);
